1b52c0a9b2afdcbe0e9d84bb15b1182c077cbc13
pritesh-mendapara <pritesh16986@gmail.com>
2025-03-10 00:27:07 +0530

    feat: project setup

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+    variable: "--font-poppins",
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+});
+
+export const metadata = {
+    title: "Kelvy Overseas",
+    description:
+        "growing for a better tomorrow view products read more Our Services Kelvy Overseas – Your Trusted Partner for Quality GoodsWelcome to Kelvy Overseas! We’re your go-to partner for sourcing top-quality goods from around the world. We offer a diverse selection of products, including pulses, fresh fruits and vegetables, premium paper items, agricultural products, and 100%",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={`${poppins.variable} antialiased`}>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+    return (
+        <div>
+            <Button>Hello</Button>
+        </div>
+    );
+};
+
+export default page;
