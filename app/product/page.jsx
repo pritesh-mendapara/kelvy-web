149f9039379c6e097fef185b1e1f147e0b5c9929
pritesh-mendapara <pritesh16986@gmail.com>
2025-06-02 21:18:24 +0530

    fix: build

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import ProductPage from "./ProductPage";
 
 const Page = () => {
-    return <ProductPage />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ProductPage />;
+        </Suspense>
+    );
 };
 
 export default Page;
